e774bef7c7c4b72c749232a11c621b67a7beb7a1
LuanFerreira97 <luan.sousa1997@hotmail.com>
2022-10-22 16:22:53 -0300

    Continuação do primeiro ex

---
+++ desafio-combustivel2.js
@@ -0,0 +1,30 @@
+
+
+// 1 - preço do etanal
+// 2 - preço da gasolina
+// 3 - tipo de combustivel
+// 4 - consumo por km do carro
+// 5 - distancia da viagem
+
+
+const precoEtanol = 4.20;
+const precoGasolina = 5.79;
+const kmPorLitros = 11.5;
+const distanciaEmKm = 1666;
+const tipoCombustivel = 'Gasolina';
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+const valorGasto = litrosConsumidos * precoEtanol;
+
+if (tipoCombustivel === 'Etanol') {
+    const valorGasto = litrosConsumidos * precoEtanol;
+    console.log(valorGasto.toFixed(2));
+    }
+
+else {
+    const valorGasto = litrosConsumidos * precoGasolina;
+    console.log(valorGasto.toFixed(2));
+}
+
+console.log(valorGasto.toFixed(2));
+
